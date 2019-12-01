7f4f7a20740486a14ec2c7e9d2e070f89e30052b
Edward M <edwardtmuniz@gmail.com>
2019-11-30 18:29:54 -0700

    clean up and converting to php to use includes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+	<?php require_once('inc/header.inc.html'); ?>
+	</head>
+	<body>
+		<div class="reveal">
+			<div class="slides">
+				<section>Slide 1</section>
+				<section>Slide 2</section>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,15 @@
+const setup = () => {
+  // More info about config & dependencies:
+  // - https://github.com/hakimel/reveal.js#configuration
+  // - https://github.com/hakimel/reveal.js#dependencies
+  Reveal.initialize({
+    dependencies: [
+      { src: "plugin/markdown/marked.js" },
+      { src: "plugin/markdown/markdown.js" },
+      { src: "plugin/notes/notes.js", async: true },
+      { src: "plugin/highlight/highlight.js", async: true }
+    ]
+  });
+};
+
+window.onload = setup;
